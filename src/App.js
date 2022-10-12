4a54352edb2c2dd4dc589d83847fcf7442d31c6d
nadimahmed1 <nadimarafat26@gmail.com>
2022-10-12 06:59:00 +0600

    homePage added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Homepage from './components/Homepage/Homepage'
 
 const App = () => {
   return (
     <div>
-      <h1>Crypto Cafe Starter Template</h1>
+      <Routes>
+        <Route path='/' element={<Homepage></Homepage>}></Route>
+      </Routes>
     </div>
   )
 }
